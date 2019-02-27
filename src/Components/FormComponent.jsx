592f7ba5081e30c38ddee345013756d5f6f0c3b3
Albert Łuniewicz <albert2046@wp.pl>
2019-02-27 19:31:56 +0100

    fetch na 2 dni wstecz - opady

--- src/Components/FormComponent.jsx
+++ src/Components/FormComponent.jsx
@@ -8,7 +8,8 @@ export default class FormComponent extends Component {
             place: 'Zielona Góra',
             date: '2019-02-28T00:00',
             cityLat: '',
-            cityLon: ''
+            cityLon: '',
+            precipProbabilityOne: ''
         }
     }
 
@@ -28,38 +29,85 @@ export default class FormComponent extends Component {
         event.preventDefault()
         const url = 'https://graphhopper.com/api/1/geocode?key=1e78f6b4-82e6-4c73-8616-f415c4ee623d&q=' + this.state.place
         fetch(url).then(r => r.json()).then(obj => {
-            console.log(obj)
+            //console.log(obj)
             this.setState({
                 cityLat: `${obj.hits[0].point.lat}`,
                 cityLon: `${obj.hits[0].point.lng}`,
             }, () => {
                 this.fetchPastWeatherOneDayAgo(event)
+                this.fetchPastWeatherTwoDayAgo(event)
             })
-            console.log(this.state.cityLat)
-            console.log(this.state.cityLon)
-
+            // console.log(this.state.cityLat)
+            // console.log(this.state.cityLon)
         }).catch(err => {
             console.log(err)
         })
     }
 
     fetchPastWeatherOneDayAgo = event => {
-        // event.preventDefault()
-        //const now = new Date()
         const searchDate = new Date(this.state.date)
-        const searchDateNumber = Math.round(searchDate.getTime() / 1000)
-        console.log(searchDateNumber)
-        console.log(this.state.cityLat)
-        console.log(this.state.cityLon)
+        const searchDateNumber = Math.round(searchDate.getTime() / 1000) - 86400
         const url = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/fdc3afa63c316609cb321c3c33772d51/' + this.state.cityLat + ',' + this.state.cityLon + ',' + searchDateNumber
         fetch(url).then(r => r.json()).then(obj => {
-            console.log('tam', obj)
-            // this.setState({
-            //     cityLat: `${obj.hits[0].point.lat}`,
-            //     cityLon: `${obj.hits[0].point.lng}`
-            // })
-            // console.log(this.state.cityLat)
-            // console.log(this.state.cityLon)
+            console.log(obj)
+            const precipProbabArray = []
+            for (let i = 0; i < 24; i++) {
+                if (obj.hourly.data[i].precipProbability > 0.15) {
+                    precipProbabArray.push(obj.hourly.data[i].precipProbability)
+                }
+            }
+            console.log(precipProbabArray)
+            if (precipProbabArray.length === 0) {
+                this.setState({
+                    precipProbabilityOne: '1 nie padało'
+                })
+            }
+            else if (precipProbabArray.length > 0 && precipProbabArray.length < 4) {
+                this.setState({
+                    precipProbabilityOne: '1 trochę padało'
+                })
+            }
+            else {
+                this.setState({
+                    precipProbabilityOne: '1 padało'
+                })
+            }
+            console.log(this.state.precipProbabilityOne)
+
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    fetchPastWeatherTwoDayAgo = event => {
+        const searchDate = new Date(this.state.date)
+        const searchDateNumber = Math.round(searchDate.getTime() / 1000) - 172800
+        const url = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/fdc3afa63c316609cb321c3c33772d51/' + this.state.cityLat + ',' + this.state.cityLon + ',' + searchDateNumber
+        fetch(url).then(r => r.json()).then(obj => {
+            console.log(obj)
+            const precipProbabArray = []
+            for (let i = 0; i < 24; i++) {
+                if (obj.hourly.data[i].precipProbability > 0.15) {
+                    precipProbabArray.push(obj.hourly.data[i].precipProbability)
+                }
+            }
+            console.log(precipProbabArray)
+            if (precipProbabArray.length === 0) {
+                this.setState({
+                    precipProbabilityTwo: '2 nie padało'
+                })
+            }
+            else if (precipProbabArray.length > 0 && precipProbabArray.length < 4) {
+                this.setState({
+                    precipProbabilityTwo: '2 trochę padało'
+                })
+            }
+            else {
+                this.setState({
+                    precipProbabilityTwo: '2 padało'
+                })
+            }
+            console.log(this.state.precipProbabilityTwo)
 
         }).catch(err => {
             console.log(err)
